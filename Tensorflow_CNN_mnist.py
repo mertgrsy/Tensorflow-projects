cb5427e4667da7a725c1aaf673ef91ab31fa79ae
Mert Gürsoy <61017164+mertgrsy@users.noreply.github.com>
2023-08-24 12:58:23 +0300

    Add files via upload

---
+++ Tensorflow_CNN_mnist.py
@@ -0,0 +1,61 @@
+import tensorflow as tf
+
+from tensorflow.keras import datasets, layers, models, activations
+import matplotlib.pyplot as plt
+
+print(tf.config.list_physical_devices())
+
+(train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
+
+# Normalize pixel values to be between 0 and 1
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+class_names = ['0', '1', '2', '3', '4',
+               '5', '6', '7', '8', '9']
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i])
+    plt.xlabel(class_names[train_labels[i]])
+plt.show()
+
+input_shape = (28, 28, 1)
+
+# convolutional base
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(32, (5,5), padding='same', activation='relu', input_shape=input_shape),
+    tf.keras.layers.Conv2D(32, (5,5), padding='same', activation='relu'),
+    tf.keras.layers.MaxPool2D(),
+    tf.keras.layers.Dropout(0.25),
+    tf.keras.layers.Conv2D(64, (3,3), padding='same', activation='relu'),
+    tf.keras.layers.Conv2D(64, (3,3), padding='same', activation='relu'),
+    tf.keras.layers.MaxPool2D(strides=(2,2)),
+    tf.keras.layers.Dropout(0.25),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+model.summary()
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+history = model.fit(train_images, train_labels, epochs=10, 
+                    validation_data=(test_images, test_labels))
+
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+plt.show()
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+print(test_acc)
